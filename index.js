996d58bcf516086c091fc8a8cee459c99e156abb
Siriusb24 <Siriusb2324@gmail.com>
2021-09-14 15:57:04 +0300

    update

--- index.js
+++ index.js
@@ -8,14 +8,37 @@ const colors = [
   ];
 
 const body = document.querySelector('body')
-console.log('body')
 const startBtn = document.querySelector('[data-action="start"]')
 const stopBtn = document.querySelector('[data-action="stop"]')
 
 startBtn.addEventListener('click', start)
 stopBtn.addEventListener('click', stop)
 
+function rundomColorBody(color){
+    body.style.backgroundColor = color;
+}
+
+let randomInterval = undefined
+
+function start(){
+    randomInterval = setInterval(randomNumber => {
+        randomNumber = randomIntegerFromInterval(0, 6);
+        rundomColorBody(colors[randomNumber])
+    }, 1000)
+    stopBtn.removeAttribute('disabled')
+    startBtn.setAttribute('disabled', true)
+}
+
+function stop(){
+    clearInterval(randomInterval)
+    startBtn.removeAttribute('disabled')
+    stopBtn.setAttribute('disabled', true)
+    body.style.backgroundColor = "#ffffff";
+}
 
 const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
+
+
+
